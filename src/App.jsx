4c991174ee97d0b6b6337dc21f6651f4c3fa45eb
shakin-shahria <shakinshaharia@gmail.com>
2024-12-26 00:55:15 +0600

    Updated the Api Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import SideBar from "./components/SideBar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+
+  function handleToggleModal() {
+    setShowModal(!showModal);
+  }
+
+  useEffect(() => {
+    async function fetchAPIData() {
+      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
+      const url = `https://api.nasa.gov/planetary/apod?api_key=${NASA_KEY}`;
+
+      try {
+        const res = await fetch(url);
+        const apiData = await res.json();
+        setData(apiData);
+      } catch (err) {
+        console.log("Error fetching data:", err.message);
+      }
+    }
+
+    fetchAPIData();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {data && <Main data={data} />}
+      {showModal && <SideBar data={data} handleToggleModal={handleToggleModal} />}
+      {data && <Footer data={data} handleToggleModal={handleToggleModal} />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
